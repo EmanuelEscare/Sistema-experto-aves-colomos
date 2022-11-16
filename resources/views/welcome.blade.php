dbcd3087475a36a7001e39f977e2a9836df37b3e
EmanuelEscare <emaescov@gmail.com>
2022-11-16 12:49:39 -0600

    b1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 @extends('layout')
 @section('content')
     <div class="container mt-5 mb-5">
-        <livewire:rules-explanation />
+    <livewire:desarrollo-nuevas-aves />
+
+        {{-- <livewire:rules-explanation /> --}}
     </div>
 @endsection
