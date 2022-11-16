e66103292e6216582c3d9fd455b463463b207d19
EmanuelEscare <emaescov@gmail.com>
2022-11-16 12:51:12 -0600

    b3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,5 @@
 @extends('layout')
 @section('content')
     <div class="container mt-5 mb-5">
-        <livewire:create-ave />
-        {{-- <livewire:rules-explanation /> --}}
     </div>
 @endsection
